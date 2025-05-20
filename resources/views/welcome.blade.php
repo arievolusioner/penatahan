7b599761145db340d10c280c77d95c154aa9a00b
Ari <ariputra.vd@gmail.com>
2025-05-20 21:25:51 +0800

    update PBL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,16 @@
         <img src="{{ asset('img/main-3.png') }}" alt="Hero Image" class="w-full h-auto object-cover">
     </div>
 
-    
+   <div class="text-center mt-12 px-5">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+        Temukan ketenangan alami bersama <span class="text-blue-600 dark:text-blue-500">suasana terbaik</span> untuk hari Anda.
+    </h1>
+    <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        Nikmati kenyamanan, kehangatan, dan kedamaian yang menyatu dalam setiap momen.
+    </p>
+    <div class="mx-auto mt-4 w-32 h-1 bg-blue-500 rounded"></div>
+</div>
+
+
+
 </x-app-layout>
